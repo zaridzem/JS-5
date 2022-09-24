0773b0276402e70322d276e2fee27dcfbffd5739
zaridzem <mariamzaridze40@gmail.com>
2022-09-24 21:20:10 +0400

    finish

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let bar = document.getElementById("burgerbar");
+let navbar = document.getElementById("nav");
+
+bar.addEventListener("click", function () {
+  navbar.classList.toggle("nav-active");
+  bar.classList.toggle("li-active");
+});
+
+// to-do list logic
+let input = document.querySelector("#input-block");
+let formelement = document.querySelector(".form-wrapper");
+let deletebtn = document.querySelector("#deleteall");
+let add = document.querySelector("#addbtn");
+let ullist = document.querySelector(".ul-list");
+
+formelement.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let value = input.value;
+  if (value == " ") {
+    return;
+  }
+  let li = document.createElement("li");
+  li.classList.add("li-active");
+
+  let deleteone = document.createElement("i");
+  deleteone.classList.add("fa-solid", "fa-trash", "delete1");
+  deleteone.addEventListener("click", function () {
+    li.remove();
+  });
+
+  li.innerText = value;
+  li.appendChild(deleteone);
+  ullist.appendChild(li);
+  input.value = "";
+});
+deletebtn.addEventListener("click", function () {
+  ullist.innerHTML = "";
+});
